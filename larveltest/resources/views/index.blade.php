a6c654982e20f5784d31058c670fdea7f82a3937
case109504 <case109504@gmail.com>
2020-07-06 17:11:27 +0800

    修改CSS（半壞）&連結頁面

--- larveltest/resources/views/index.blade.php
+++ larveltest/resources/views/index.blade.php
@@ -77,19 +77,18 @@
 								while($row = $res->fetch_assoc()) {
 									// echo '<td>'.$row["video_id"]."".$row["video_name"]."".$row["time"].'</td></br>';   //輸出結果
 									echo '<div class="box">
-									<a href="{{ route(  '.'introduction'.') }}?video_name='.$row["video_name"].'" class="image fit" data-poptrox="ignore"><img src="'.$row["videopicture"].'" alt="" /></a>
+									<a href="introduction?video_name='.$row["video_name"].'" class="image fit" data-poptrox="ignore"><img src="'.$row["videopicture"].'" alt="" /></a>
 									<div class="inner">
 										<h3>'.$row["video_name"].'</h3>
 										<p>主演： '.$row["actor_name"].'<br /></p>
 										<p>評分： '.$row["score"].' <br /></p>
-										<a href="{{ route( "introduction") }}?video_name='.$row["video_name"].'" class="button fit" data-poptrox="ignore">影片介紹</a>
+										<a href="introduction?video_name='.$row["video_name"].'" class="button fit" data-poptrox="ignore">影片介紹</a>
 									</div>
 								</div>';
 								}
 							//}
 							$cn->close();
 							?>
-							
 
 						</div>
 
--- larveltest/resources/views/introduction.blade.php
+++ larveltest/resources/views/introduction.blade.php
@@ -4,7 +4,7 @@
 		<title>Full Motion</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="css/main.css" />
 	</head>
 	<body id="top">
 	<?php
@@ -111,12 +111,12 @@
 				</footer>
 
 		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.poptrox.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+			<script src="js/jquery.min.js"></script>
+			<script src="js/jquery.scrolly.min.js"></script>
+			<script src="js/jquery.poptrox.min.js"></script>
+			<script src="js/skel.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
 
 
 
