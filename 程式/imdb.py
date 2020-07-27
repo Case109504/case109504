ad4686612ac96015d40b7ac30dfc85925a065066
Case109504 <63043786+Case109504@users.noreply.github.com>
2020-07-27 17:18:33 +0800

    imdb web crawler

---
+++ 程式/imdb.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import requests
+from lxml import etree
+import re
+import pymongo
+import time
+client = pymongo.MongoClient('localhost', 27017)
+test = client['test']
+video2 = test['video2']
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+}
+# Download IMDB's Top 250 data
+url = 'http://www.imdb.com/chart/top'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+
+movies = soup.select('td.titleColumn')
+links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
+crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
+ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
+votes = [c.attrs.get('data-value') for c in soup.select('td.ratingColumn strong')]
+
+imdb = []
+
+# Store each item into dictionary (data), then put those into a list (imdb)
+for index in range(0, len(movies)):
+    # Seperate movie into: 'place', 'title', 'year'
+    movie_string = movies[index].get_text()
+    movie = (' '.join(movie_string.split()).replace('.', ''))
+    movie_title = movie[len(str(index))+1:-7]
+    year = re.search('\((.*?)\)', movie_string).group(1)
+    place = movie[:len(str(index))-(len(movie))]
+    data = {"movie_title": movie_title,
+            "year": year,
+            "place": place,
+            "star_cast": crew[index],
+            "rating": ratings[index],
+            #"vote": votes[index],
+            "link": links[index]}
+    imdb.append(data)
+    
+    video2.insert_one(data)
+
+for item in imdb:
+    print(item['place'], '-', item['movie_title'], '('+item['year']+') -', 'Starring:', item['star_cast'], item['rating'])
+    
+
+    
+  
