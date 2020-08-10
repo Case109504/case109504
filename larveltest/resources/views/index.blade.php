120b72f7892273d602fcf2f04338b46822c72739
case109504 <case109504@gmail.com>
2020-08-10 18:56:46 +0800

    把連結影片介紹的頁面之DB與法修正

--- larveltest/resources/views/index.blade.php
+++ larveltest/resources/views/index.blade.php
@@ -7,47 +7,6 @@
 		<link rel="stylesheet" href="css/main2.css" />
 	</head>
 	<body id="top">
-	<?php
-		$hostname = "140.131.115.87:3306";
-		$username = "root";
-		$password = "109504109504";
-		$databasename = "testdb";
-		
-		// 創建連接
-		$cn = new mysqli($hostname,$username,$password,$databasename);
-		
-		if (!$cn)//判斷連線是否為空
-		{
-		die("連線錯誤: " . mysqli_connect_error());//連線失敗 列印錯誤日誌
-		}
-		$cn->query("SET NAMES utf8");//設定 字符集為utf8格式
-		$cn->select_db("Video");//選擇要操作的資料表
-
-		$sql="select distinct Video.video_id, Video.video_name , Video.videopicture , type.type_name , director.director_name, actor_name, screenwriter_name, source_name, plot_name, area_name, awards_name, film_source ,type_name,score,comments_name from testdb.Video
-		left join testdb.type on Video.type_id = type.type_id 
-		left join testdb.director_record on Video.video_id = director_record.video_id
-		left join testdb.director on director.director_id = director_record.director_id
-		left join testdb.actor_record on Video.video_id = actor_record.video_id
-		left join testdb.actor on actor.actor_id = actor_record.actor_id
-		left join testdb.screenwriter_record on Video.video_id = screenwriter_record.video_id
-		left join testdb.screenwriter on screenwriter.screenwriter_id = screenwriter_record.screenwriter_id
-		left join testdb.access on Video.video_id = access.video_id
-		left join testdb.sources on sources.source_id = access.source_id
-		left join testdb.plot_record on Video.video_id = plot_record.video_id
-		left join testdb.plot on plot.plot_id = plot_record.plot_id
-		left join testdb.area on Video.area_id = area.area_id
-		left join testdb.awards on Video.video_id = awards.video_id
-		left join testdb.film_source on Video.video_id = film_source.video_id
-		left join testdb.score on Video.video_id = score.video_id
-		left join testdb.comments on Video.video_id = comments.video_id
-		group by video_id
-		";
-		mysqli_query($cn,$sql);    //傳入資料庫連線引數，sql字串。
-		$res=$cn->query($sql);    //接收查詢產生的結果集
-		    //將結果集賦值給陣列物件
-		
-
-?>
 			<!-- Banner -->
 			<!--
 				To use a video as your background, set data-video to the name of your video without
@@ -73,21 +32,36 @@
 						<div class="thumbnails">
 
 							<?php 
-							//while($res->num_rows > 0){可改if
-								while($row = $res->fetch_assoc()) {
-									// echo '<td>'.$row["video_id"]."".$row["video_name"]."".$row["time"].'</td></br>';   //輸出結果
-									echo '<div class="box">
-									<a href="introduction?video_name='.$row["video_name"].'" class="image fit" data-poptrox="ignore"><img src="'.$row["videopicture"].'" alt="" /></a>
+							use Illuminate\Support\Facades\DB;
+							$Video = DB::table('Video')
+							->leftJoin('type', 'Video.type_id', '=', 'type.type_id')
+							->leftJoin('director_record', 'Video.video_id', '=', 'director_record.video_id')
+							->leftJoin('director', 'director.director_id', '=', 'director_record.director_id')
+							->leftJoin('actor_record', 'Video.video_id', '=', 'actor_record.video_id')
+							->leftJoin('actor', 'actor.actor_id', '=', 'actor_record.actor_id')
+							->leftJoin('screenwriter_record', 'Video.video_id', '=', 'screenwriter_record.video_id')
+							->leftJoin('screenwriter', 'screenwriter.screenwriter_id', '=', 'screenwriter_record.screenwriter_id')
+							->leftJoin('access', 'Video.video_id', '=', 'access.video_id')
+							->leftJoin('sources', 'sources.source_id', '=', 'access.source_id')
+							->leftJoin('plot_record', 'Video.video_id', '=', 'plot_record.video_id')
+							->leftJoin('plot', 'plot.plot_id', '=', 'plot_record.plot_id')
+							->leftJoin('area', 'Video.area_id', '=', 'area.area_id')
+							->leftJoin('awards', 'Video.video_id', '=', 'awards.video_id')
+							->leftJoin('film_source', 'Video.video_id', '=', 'film_source.video_id')
+							->leftJoin('score', 'Video.video_id', '=', 'score.video_id')
+							->get();
+							foreach ($Video as $Video)
+							{
+								echo '<div class="box">
+									<a href="introduction?video_name='.$Video->video_name.'" class="image fit" data-poptrox="ignore"><img src="'.$Video->videopicture.'" alt="" /></a>
 									<div class="inner">
-										<h3>'.$row["video_name"].'</h3>
-										<p>主演： '.$row["actor_name"].'<br /></p>
-										<p>評分： '.$row["score"].' <br /></p>
-										<a href="introduction?video_name='.$row["video_name"].'" class="button fit" data-poptrox="ignore">影片介紹</a>
+										<h3>'.$Video->video_name.'</h3>
+										<p>主演： '.$Video->actor_name.'<br /></p>
+										<p>評分： '.$Video->score.' <br /></p>
+										<a href="introduction?video_name='.$Video->video_name.'" class="button fit" data-poptrox="ignore">影片介紹</a>
 									</div>
 								</div>';
-								}
-							//}
-							$cn->close();
+							}
 							?>
 
 						</div>
