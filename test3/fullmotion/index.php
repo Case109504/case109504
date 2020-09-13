2f90eb67455ba6ead126b7a4550e033517e53f40
case109504 <case109504@gmail.com>
2020-09-13 15:28:28 +0800

    試寫另一個資料庫的輸出

--- test3/fullmotion/index.php
+++ test3/fullmotion/index.php
@@ -1,4 +1,7 @@
 <!DOCTYPE HTML>
+<?php
+include '../php/DataBase.php';
+?>
 <html lang = "zh-tw">
 	<head>
 		<title>Full Motion</title>
@@ -8,46 +11,13 @@
 	</head>
 	<body id="top">
 	<?php
-		$hostname = "140.131.115.87:3306";
-		$username = "root";
-		$password = "109504109504";
-		$databasename = "testdb";
-		
-		// 創建連接
-		$cn = new mysqli($hostname,$username,$password,$databasename);
-		
-		if (!$cn)//判斷連線是否為空
-		{
-		die("連線錯誤: " . mysqli_connect_error());//連線失敗 列印錯誤日誌
-		}
-		$cn->query("SET NAMES utf8");//設定 字符集為utf8格式
-		$cn->select_db("Video");//選擇要操作的資料表
-
-		$sql="select distinct Video.video_id, Video.video_name , Video.videopicture , type.type_name , director.director_name, actor_name, screenwriter_name, source_name, plot_name, area_name, awards_name, film_source ,type_name,score,comments_name from testdb.Video
-		left join testdb.type on Video.type_id = type.type_id 
-		left join testdb.director_record on Video.video_id = director_record.video_id
-		left join testdb.director on director.director_id = director_record.director_id
-		left join testdb.actor_record on Video.video_id = actor_record.video_id
-		left join testdb.actor on actor.actor_id = actor_record.actor_id
-		left join testdb.screenwriter_record on Video.video_id = screenwriter_record.video_id
-		left join testdb.screenwriter on screenwriter.screenwriter_id = screenwriter_record.screenwriter_id
-		left join testdb.access on Video.video_id = access.video_id
-		left join testdb.sources on sources.source_id = access.source_id
-		left join testdb.plot_record on Video.video_id = plot_record.video_id
-		left join testdb.plot on plot.plot_id = plot_record.plot_id
-		left join testdb.area on Video.area_id = area.area_id
-		left join testdb.awards on Video.video_id = awards.video_id
-		left join testdb.film_source on Video.video_id = film_source.video_id
-		left join testdb.score on Video.video_id = score.video_id
-		left join testdb.comments on Video.video_id = comments.video_id
-		group by video_id
-		";
-		mysqli_query($cn,$sql);    //傳入資料庫連線引數，sql字串。
-		$res=$cn->query($sql);    //接收查詢產生的結果集
-		    //將結果集賦值給陣列物件
-		
-
-?>
+		$db = DB1();
+		$sql="SELECT * FROM testdb1.video
+		left join testdb1.area on video.area_id = area.area_id
+		where area_name = '" . $_GET["area_name"]."'";
+		$result = $db->query($sql);
+		$row = $result->fetch(PDO::FETCH_ASSOC);
+	?>
 			<!-- Banner -->
 			<!--
 				To use a video as your background, set data-video to the name of your video without
@@ -73,22 +43,29 @@
 						<div class="thumbnails">
 
 							<?php 
-							//while($res->num_rows > 0){可改if
-								while($row = $res->fetch_assoc()) {
-									// echo '<td>'.$row["video_id"]."".$row["video_name"]."".$row["time"].'</td></br>';   //輸出結果
-									echo '<div class="box">
-									<a href="introduction.php?video_name='.$row["video_name"].'" class="image fit" data-poptrox="ignore"><img src="'.$row["videopicture"].'" alt="" /></a>
+							while($row = $result->fetch()){ 
+								echo '<div class="box">
+									
 									<div class="inner">
 										<h3>'.$row["video_name"].'</h3>
-										<p>主演： '.$row["actor_name"].'<br /></p>
-										<p>評分： '.$row["score"].' <br /></p>
+										<p>主演： ';
+										$sql2="SELECT * FROM testdb1.video
+										left join testdb1.area on video.area_id = area.area_id
+										left join testdb1.actor_record on video.video_id = actor_record.video_id
+										left join testdb1.actor on actor_record.actor_id = actor.actor_id
+										where area_name = '" . $_GET["area_name"]."' and video_name = '" . $row["video_name"]."'";
+										$result2 = $db->query($sql2);
+										$row2 = $result2->fetch(PDO::FETCH_ASSOC);
+								while($row2 = $result2->fetch()){ 
+								echo $row2["actor_name"].'<br />';
+								echo '</p>
 										<a href="introduction.php?video_name='.$row["video_name"].'" class="button fit" data-poptrox="ignore">影片介紹</a>
 									</div>
 								</div>';
 								}
-							//}
-							$cn->close();
+							}
 							?>
+							
 							
 
 						</div>
