111467c20b70d40a6a2560c0b4673bff84b52024
case109504 <case109504@gmail.com>
2020-09-23 19:53:20 +0800

    更換資料庫並修正顯示方式

--- test3/video_select.php
+++ test3/video_select.php
@@ -16,41 +16,12 @@ if ($_SESSION["acc"] == "") {
 	</head>
 	<body class="subpage">
 		<?php
-			$hostname = "140.131.115.87:3306";
-			$username = "root";
-			$password = "109504109504";
-			$databasename = "testdb";
-			
-			// 創建連接
-			$cn = new mysqli($hostname,$username,$password,$databasename);
-			
-			if (!$cn)//判斷連線是否為空
-			{
-			die("連線錯誤: " . mysqli_connect_error());//連線失敗 列印錯誤日誌
-			}
-			$cn->query("SET NAMES utf8");//設定 字符集為utf8格式
-			$cn->select_db("Video");//選擇要操作的資料表
-
-			$sql="select distinct Video.video_id, Video.video_name , Video.videopicture , type.type_name , director.director_name, actor_name, screenwriter_name, source_name, plot_name, area_name, awards_name, film_source ,type_name,score,comments_name from testdb.Video
-			left join testdb.type on Video.type_id = type.type_id 
-			left join testdb.director_record on Video.video_id = director_record.video_id
-			left join testdb.director on director.director_id = director_record.director_id
-			left join testdb.actor_record on Video.video_id = actor_record.video_id
-			left join testdb.actor on actor.actor_id = actor_record.actor_id
-			left join testdb.screenwriter_record on Video.video_id = screenwriter_record.video_id
-			left join testdb.screenwriter on screenwriter.screenwriter_id = screenwriter_record.screenwriter_id
-			left join testdb.access on Video.video_id = access.video_id
-			left join testdb.sources on sources.source_id = access.source_id
-			left join testdb.plot_record on Video.video_id = plot_record.video_id
-			left join testdb.plot on plot.plot_id = plot_record.plot_id
-			left join testdb.area on Video.area_id = area.area_id
-			left join testdb.awards on Video.video_id = awards.video_id
-			left join testdb.film_source on Video.video_id = film_source.video_id
-			left join testdb.score on Video.video_id = score.video_id
-			left join testdb.comments on Video.video_id = comments.video_id
-			group by video_id"; 
-			mysqli_query($cn,$sql);    //傳入資料庫連線引數，sql字串。
-			$res=$cn->query($sql);    //接收查詢產生的結果集
+			$db = DB1();
+			$sql="SELECT * FROM testdb1.video
+			left join testdb1.area on video.area_id = area.area_id";
+			$result = $db->query($sql);
+			$row = $result->fetch(PDO::FETCH_ASSOC);
+			$result->execute();
 		?>
 
 		<!-- Header -->
@@ -105,18 +76,17 @@ if ($_SESSION["acc"] == "") {
 						<input name="submit" type="button" id="submit" onclick="location.href='php/logOut.php'" value="登出" />
 						<P></P>
 						<table>
-							<tr><th>影片編號</th><th>影片名稱</th><th>主演</th><th>類型</th><th>導演</th><th>編劇</th><th>修改/刪除</th></tr>
+							<tr><th>影片編號</th><th>影片繁中</th><th>影片英文</th><th>影片簡中</th><th>區域</th><th>介紹</th><th>修改/刪除</th></tr>
 						<?php
-						while($row = $res->fetch_assoc()) {
+						while($row = $result->fetch()) {
 							echo "<tr><td width='5%'>".$row["video_id"]."</td>
-							<td width='15%'>".$row["video_name"]."</td>
-							<td width='10%'>".$row["actor_name"]."</td>
-							<td width='10%'>".$row["type_name"]."</td>
-							<td width='10%'>".$row["director_name"]."</td>
-							<td width='10%'>".$row["screenwriter_name"]."</td>
+							<td width='5%'>".$row["video_name"]."</td>
+							<td width='5%'>".$row["video_eg_name"]."</td>
+							<td width='5%'>".$row["video_ch_name"]."</td>
+							<td width='10%'>".$row["area_name"]."</td>
+							<td width='60%'>".$row["introduction"]."</td>
 							<td width='10%'><a href='video_update.php?video_id=".$row["video_id"]."'>修改</a> <a href='video_delete.php?video_id=".$row["video_id"]."'>刪除</a></td></tr>";
 						}
-						$cn->close();
 						?>
 						</table>
 						</body>
