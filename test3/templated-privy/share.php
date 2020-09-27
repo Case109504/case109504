a4cf300bdc6e22f2527aad5a77b70a8425ef5320
csr50524 <sharon50524@gmail.com>
2020-09-28 00:40:45 +0800

    修改連到FindOrder的路徑

--- test3/templated-privy/share.php
+++ test3/templated-privy/share.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
 session_start();
-include 'php/FindOrder.php';
+include '../php/FindOrder.php';
 if ($_SESSION["acc"] == "") {
     header('Location: ../home.html');
     $_SESSION["unLog"] = true;
