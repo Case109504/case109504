5df2f07d49260322a48e97c7660d45b1fc7e277a
case109504 <case109504@gmail.com>
2020-10-11 17:25:56 +0800

    同好推薦編寫

--- test3/templated-privy/samerecommend.php
+++ test3/templated-privy/samerecommend.php
@@ -13,6 +13,7 @@ if ($_SESSION["acc"] == "") {
 <title></title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
+<meta name="referrer" content="never">
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
 <link href="default.css" rel="stylesheet" type="text/css" media="all" />
 <link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
@@ -33,6 +34,8 @@ if ($_SESSION["acc"] == "") {
 			session_unset();
 		}   
 	}
+
+	
 ?>
 <div id="page" class="container">
 	<div id="header">
@@ -72,28 +75,76 @@ if ($_SESSION["acc"] == "") {
 				<h2>Maecenas lectus sapien</h2>
 				<span class="byline">Integer sit amet aliquet pretium</span>
 			</div>
-			<ul class="style1">
-				<li class="first">
-					<p class="date"><a href="#">Jan<b>05</b></a></p>
-					<h3>Amet sed volutpat mauris</h3>
-					<p><a href="#">Consectetuer adipiscing elit. Nam pede erat, porta eu, lobortis eget, tempus et, tellus. Etiam neque. Vivamus consequat lorem at nisl. Nullam non wisi a sem semper eleifend. Etiam non felis. Donec ut ante.</a></p>
-				</li>
-				<li>
-					<p class="date"><a href="#">Jan<b>03</b></a></p>
-					<h3>Sagittis diam dolor amet</h3>
-					<p><a href="#">Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Mauris quam enim, molestie. Donec leo, vivamus fermentum nibh in augue praesent congue rutrum.</a></p>
-				</li>
-				<li>
-					<p class="date"><a href="#">Jan<b>01</b></a></p>
-					<h3>Amet sed volutpat mauris</h3>
-					<p><a href="#">Consectetuer adipiscing elit. Nam pede erat, porta eu, lobortis eget, tempus et, tellus. Etiam neque. Vivamus consequat lorem at nisl. Nullam non wisi a sem semper eleifend. Etiam non felis. Donec ut ante.</a></p>
-				</li>
-				<li>
-					<p class="date"><a href="#">Dec<b>31</b></a></p>
-					<h3>Sagittis diam dolor amet</h3>
-					<p><a href="#">Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Mauris quam enim, molestie. Donec leo, vivamus fermentum nibh in augue praesent congue rutrum.</a></p>
-				</li>
-			</ul>
+			<?php 
+				$db = DB1();
+				$sql4="SELECT * FROM testdb1.type_sort
+				left join testdb1.vtype on type_sort.vtype_name = vtype.vtype_name
+				where account = '" . $_SESSION["acc"]."'
+				order by sort desc
+				limit 3";
+				$result4 = $db->query($sql4);
+				$row4 = $result4->fetch(PDO::FETCH_ASSOC);
+				$result4->execute();
+				while($row4 = $result4->fetch()){ 
+					//echo "1<br/>";
+					$sql5="SELECT * FROM testdb1.type_sort
+					left join testdb1.vtype on type_sort.vtype_name = vtype.vtype_name
+					where account != '" . $_SESSION["acc"]."' and sort > 1
+					order by sort desc";
+					$result5 = $db->query($sql5);
+					$row5 = $result5->fetch(PDO::FETCH_ASSOC);
+					$result5->execute();
+					while($row5 = $result5->fetch()){ 
+						//echo "2<br/>".$row4["vtype_name"].$row5["vtype_name"];
+						if($row4["vtype_id"]==$row5["vtype_id"]){
+							$sql="SELECT * FROM testdb1.member_video_list
+							left join testdb1.video on video.video_id = member_video_list.video_id
+							left join testdb1.area on video.area_id = area.area_id
+							where account != '" . $_SESSION["acc"]."'";
+							$result = $db->query($sql);
+							$row = $result->fetch(PDO::FETCH_ASSOC);
+							$result->execute();
+							while($row = $result->fetch()){ 
+								//echo "3<br/>";
+								echo '<div class="box">
+								<a href="../php/videoClickRecord.php?video_name='.$row["video_name"].'&video_id='.$row["video_id"].'&area_name='.$row["area_name"].'" class="image fit" data-poptrox="ignore"><img src="'.$row["picture"].'" height=500px></a>
+									<div class="inner">
+										<h3>'.$row["video_name"].'</h3>
+										<h4>主演： ';
+										$sql2="SELECT * FROM testdb1.video
+										left join testdb1.area on video.area_id = area.area_id
+										left join testdb1.actor_record on video.video_id = actor_record.video_id
+										left join testdb1.actor on actor_record.actor_id = actor.actor_id
+										where area_name = '" . $row["area_name"]."' and video_name = '" . $row["video_name"]."'
+										limit 5";
+										$result2 = $db->query($sql2);
+										$row2 = $result2->fetch(PDO::FETCH_ASSOC);
+										$result2->execute();
+								while($row2 = $result2->fetch()){ 
+								echo $row2["actor_name"].'<br />';
+								}
+								echo '</h4>
+										<h4>評分： ';
+										$sql3="SELECT * FROM testdb1.score
+										left join testdb1.video on score.video_id = video.video_id
+										left join testdb1.video_from on score.vfrom_id = video_from.vfrom_id
+										where video_name = '" . $row["video_name"]."'";
+										$result3 = $db->query($sql3);
+										$row3 = $result3->fetch(PDO::FETCH_ASSOC);
+										$result3->execute();
+										while($row3 = $result3->fetch()){ 
+											echo $row3["vfrom"].'：'.$row3["score"].'<br />';
+											}		
+								echo ' <br /></h4>
+										<a href="../fullmotion/introduction.php?video_name='.$row["video_name"].'&video_id='.$row["video_id"].'&area_name='.$row["area_name"].'" class="button fit" data-poptrox="ignore">影片介紹</a>
+									</div>
+								</div>';
+								
+							}
+						}
+					}
+				}
+			?>
 		</div>
 		<div id="copyright">
 			<span>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a></span>
